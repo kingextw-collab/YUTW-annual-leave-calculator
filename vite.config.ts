26b0bb2322dac776206cd84ce72524e859c05e27
kingextw-collab <kingex.tw@gmail.com>
2025-12-05 14:49:36 +0800

    Update vite.config.ts
    新增
    base: './', 
    
      plugins: [
        react({
          // 啟用 React 19 的新轉換
          jsxRuntime: 'classic',
        }),
      ],

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
+    base: './', 
+
+    plugins: [
+    react({
+      // 啟用 React 19 的新轉換
+      jsxRuntime: 'classic',
+    }),
+    ],
     const env = loadEnv(mode, '.', '');
     return {
       server: {
